96e6e6a48e988dccb7a3bee994ad93de2a5829bc
Jose <santosjosel1995@gmail.com>
2020-10-20 12:38:58 -0400

    Array of objects to map all the buttons

---
+++ src/components/buttonArrays.js
@@ -0,0 +1,73 @@
+export const numberArray = [
+  {
+    id: "zero",
+    num: 0
+  },
+  {
+    id: "one",
+    num: 1
+  },
+  {
+    id: "two",
+    num: 2
+  },
+  {
+    id: "three",
+    num: 3
+  },
+  {
+    id: "four",
+    num: 4
+  },
+  {
+    id: "five",
+    num: 5
+  },
+  {
+    id: "six",
+    num: 6
+  },
+  {
+    id: "seven",
+    num: 7
+  },
+  {
+    id: "eight",
+    num: 8
+  },
+  {
+    id: "nine",
+    num: 9
+  }
+]
+
+export const operationArray = [
+  {
+    id: "clear",
+    func: "AC",
+  },
+  {
+    id: "divide",
+    func: "/"
+  },
+  {
+    id: "multiply",
+    func: "*"
+  },
+  {
+    id: "subtract",
+    func: "-"
+  },
+  {
+    id: "add",
+    func: "+"
+  },
+  {
+    id: "decimal",
+    num: ".",
+  },
+  {
+    id: "equal",
+    num: "="
+  },
+]
