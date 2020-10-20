cbbdd318ade1834b6bfeaa0913b99d837f7cf5a6
Jose <santosjosel1995@gmail.com>
2020-10-20 15:19:29 -0400

    Added Buttons to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {numberArray, operationArray} from './buttonArrays'
+import Cbuttons from './Cbuttons';
+import {numberArray, operationArray} from './buttonArrays';
+
+const funcButtons = operationArray.map((obj) =>
+  <Cbuttons
+    id={obj.id}
+    func={obj.func}
+  />
+)
+const numButtons = numberArray.map((obj) =>
+  <Cbuttons
+    id={obj.id}
+    func={obj.num}
+  />
+)
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="cal">
+      <div className="cal-buttons">
+        {funcButtons}
+        {numButtons}
+      </div>
     </div>
   )
 }
