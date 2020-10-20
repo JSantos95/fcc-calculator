9272c803af28c6ea843195594b0944759fe4de5e
Jose <santosjosel1995@gmail.com>
2020-10-20 12:39:14 -0400

    Base app page

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {numberArray, operationArray} from './buttonArrays'
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App;
