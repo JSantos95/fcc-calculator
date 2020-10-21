060d7c940f3c318737e53ca7b722dab3a43870ae
Jose <santosjosel1995@gmail.com>
2020-10-21 11:07:56 -0400

    Tempory value added and redid the div format

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cbuttons from './Cbuttons';
+import '../styles/style.css';
 import {numberArray, operationArray} from './buttonArrays';
 
-const funcButtons = operationArray.map((obj) =>
-  <Cbuttons
-    id={obj.id}
-    func={obj.func}
-  />
-)
-const numButtons = numberArray.map((obj) =>
-  <Cbuttons
-    id={obj.id}
-    func={obj.num}
-  />
-)
-
 const App = () => {
+
+  const [value, setValue] = useState(0);
+
+  const funcButtons = operationArray.map((obj) =>
+    <Cbuttons
+      id={obj.id}
+      func={obj.func}
+    />
+  )
+  const numButtons = numberArray.map((obj) =>
+    <Cbuttons
+      id={obj.id}
+      func={obj.num}
+    />
+  )
   return (
     <div className="cal">
-      <div className="cal-buttons">
-        {funcButtons}
-        {numButtons}
+      <div className="cal-board">
+
+        <div className="cal-value">
+          {value}
+        </div>
+
+        <div className="cal-buttons">
+          {funcButtons}
+          {numButtons}
+        </div>
+
       </div>
     </div>
   )
