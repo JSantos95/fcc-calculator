eda0ae112721c63489c96962adc6e09441d47d48
Jose <santosjosel1995@gmail.com>
2020-10-21 10:55:17 -0400

    Don't need div to wrap the buttons

--- src/components/Cbuttons.js
+++ src/components/Cbuttons.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Cbuttons = (props) => {
   return (
-    <div className="cal-buttons--val">
-      <button id={props.id}>
-        {props.func}
-      </button>
-    </div>
+    <button className="cal-button--actual" id={props.id}>
+      {props.func}
+    </button>
   )
 }
 
