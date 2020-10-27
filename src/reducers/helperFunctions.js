0a925a07c663b710eaa24abd72f3152042c694fd
Jose <santosjosel1995@gmail.com>
2020-10-27 18:03:44 -0400

    New file for funcs that help reducer and keep reducer file more clean

---
+++ src/reducers/helperFunctions.js
@@ -0,0 +1,41 @@
+/*
+checks if an operator was just entered
+allows an "-" to be added after another operator
+but not two operator ex. 8 + - - 7
+*/
+export const isOperator = (arr, oper) => {
+  if(arr.length === 0) {
+    return false;
+  }
+
+  const val = arr[arr.length-1];
+  const val2 = arr[arr.length-2];
+
+  if(oper === '-') {
+    if(typeof val === 'string' && typeof val2 !== 'string'){
+      return false;
+    }
+  }
+
+  if (val === 'x' || val === '+' || val === '/' || val === '-') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//makes sure other operator don't overwrite negative operator
+export const isANegative = (arr) => {
+  if(arr.length === 1 && arr[0] === '-'){
+    return true;
+  }
+  if(arr[arr.length-1] === '-' && typeof arr[arr.length-2] === 'string'){
+    return true;
+  }
+  return false;
+}
+
+//new array of value that has its last index cut off
+export const trim = (arr) => {
+  return arr.slice(0, arr.length-1);
+}
