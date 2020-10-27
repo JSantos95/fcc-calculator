b72f507af48adaca23b0f95837e2bdb0a849bce0
Jose <santosjosel1995@gmail.com>
2020-10-26 21:12:25 -0400

    hooked buttons to correct action depending on their id and keys

--- src/components/CalButtons.js
+++ src/components/CalButtons.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import {addNumber, addOp, clearAll, decimalPoint, equals} from '../actions';
 
 const CalButtons = (props) => {
+
+  const dispatch = useDispatch();
+
+  const actionCaller = () => {
+    if(props.num){
+      dispatch(addNumber(props.num));
+    }else if (props.id === 'clear'){
+      dispatch(clearAll());
+    }else if (props.id === 'decimal'){
+      dispatch(decimalPoint(props.func));
+    }else if (props.id === 'equal'){
+      dispatch(equals(props.func));
+    }else {
+      dispatch(addOp(props.func));
+    }
+  }
+
   return (
-    <button className="cal-button--actual" id={props.id}>
+    <button className="cal-button--actual" id={props.id} onClick={actionCaller}>
       {props.func}
     </button>
   )
