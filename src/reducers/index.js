63a90c63cd4dc25eecc01f8ec3b0c29b6bc21cea
Jose <santosjosel1995@gmail.com>
2020-10-27 18:02:55 -0400

    Finished NUM and OP actions and moved non-reducer functions from file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,29 @@
 import { NUMBER, OP, CLEAR, DEC, EQ } from '../actions/actionTypes';
+import { isOperator, trim, isANegative } from './helperFunctions';
 
-const isOperator = (arr) => {
-  const val = arr[arr.length-1];
-  if (val === 'x' || val === '+' || val === '/' || val === '-'){
-    return true;
-  } else
-    return false;
-}
-
-const calValue = (value = [0], action) => {
+const calValue = (value = [], action) => {
   switch(action.type) {
     case NUMBER:
-      if(!isOperator(value)) {
-        //replacing the last num with it plus the new number added to the end
-        return value.splice(value.length-1, 1, ((value[value.length-1] * 10) + action.payload.num));
+      if(typeof value[value.length-1] === 'number') { //replaces the last num with itself plus the new number added to the end
+        return [...trim(value), ((value[value.length-1]*10) + action.payload.num)];
       } else {
         return [...value, action.payload.num];
       }
     case OP:
-      if (isOperator(value)) {
-        //new operator replaces the last one
-        return value.splice(value.length-1, 1, action.payload.func);
+      if((value.length === 0 & action.payload.func !== "-") || isANegative(value)) {
+        return value;
+      }
+      if (isOperator(value, action.payload.func)) { //new operator replaces the last one unless its "-"
+        return [...trim(value), action.payload.func];
       } else {
         return [...value, action.payload.func];
       }
     case CLEAR:
-      return value = [0];
+      return value = [];
+    case DEC:
+      return value;
+    case EQ:
+      return value;
     default:
       return value;
   }
