9159667ad5faeab7217e86818a9e8b451a048682
Jose <santosjosel1995@gmail.com>
2020-10-28 12:33:41 -0400

    Deciaml action added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { NUMBER, OP, CLEAR, DEC, EQ } from '../actions/actionTypes';
-import { isOperator, trim, isANegative } from './helperFunctions';
+import { isOperator, trim, isANegative, newDecimal, noDecimal } from './helperFunctions';
 
 const calValue = (value = [], action) => {
   switch(action.type) {
     case NUMBER:
       if(typeof value[value.length-1] === 'number') { //replaces the last num with itself plus the new number added to the end
-        return [...trim(value), ((value[value.length-1]*10) + action.payload.num)];
+        return [...trim(value), ((value[value.length-1] * 10) + action.payload.num)];
       } else {
         return [...value, action.payload.num];
       }
@@ -21,9 +21,17 @@ const calValue = (value = [], action) => {
     case CLEAR:
       return value = [];
     case DEC:
-      return value;
+      if(newDecimal(value)) {
+        return [...value, 0, '.', 0];
+      } else if (noDecimal(value)) {
+        return [...value, '.', 0];
+      } else {
+        return value;
+      }
     case EQ:
-      return value;
+      for(let i = 0; i < value.length; i++){
+
+      }
     default:
       return value;
   }
