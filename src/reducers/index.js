c2b6d44756bece7290c194435bcca7b5f48b224d
Jose <santosjosel1995@gmail.com>
2020-10-28 21:29:31 -0400

    EQ action + if statement for other actions after a EQ action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { NUMBER, OP, CLEAR, DEC, EQ } from '../actions/actionTypes';
-import { isOperator, trim, isANegative, newDecimal, noDecimal } from './helperFunctions';
+import { isOperator, trim, isANegative, newDecimal, noDecimal, doMath, isStringANum } from './helperFunctions';
 
 const calValue = (value = [], action) => {
   switch(action.type) {
     case NUMBER:
-      if(typeof value[value.length-1] === 'number') { //replaces the last num with itself plus the new number added to the end
+      if(value.length === 1 && typeof value[0] === 'string' && isStringANum(value[0])){
+        return [action.payload.num];
+      }else if (typeof value[value.length-1] === 'number') { //replaces the last num with itself plus the new number added to the end
         return [...trim(value), ((value[value.length-1] * 10) + action.payload.num)];
       } else {
         return [...value, action.payload.num];
@@ -13,6 +15,9 @@ const calValue = (value = [], action) => {
       if((value.length === 0 & action.payload.func !== "-") || isANegative(value)) {
         return value;
       }
+      if(value.length === 1 && typeof value[0] === 'string' && isStringANum(value[0])){
+        return [Number(value[0]), action.payload.func];
+      } else
       if (isOperator(value, action.payload.func)) { //new operator replaces the last one unless its "-"
         return [...trim(value), action.payload.func];
       } else {
@@ -21,7 +26,9 @@ const calValue = (value = [], action) => {
     case CLEAR:
       return value = [];
     case DEC:
-      if(newDecimal(value)) {
+      if(value.length === 1 && typeof value[0] === 'string' && isStringANum(value[0])){
+        return [Number(value[0]), '.', 0];
+      } else if(newDecimal(value)) {
         return [...value, 0, '.', 0];
       } else if (noDecimal(value)) {
         return [...value, '.', 0];
@@ -29,9 +36,7 @@ const calValue = (value = [], action) => {
         return value;
       }
     case EQ:
-      for(let i = 0; i < value.length; i++){
-
-      }
+      return doMath(value);
     default:
       return value;
   }
